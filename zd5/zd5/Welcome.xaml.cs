757baded53f3051db9a21b0be6ec2e7edc2a8e94
albedolover2992-ctrl <albedolover2992@gmail.com>
2026-06-28 13:25:53 +0500

    first commit

--- zd5/zd5/Welcome.xaml.cs
+++ zd5/zd5/Welcome.xaml.cs
@@ -12,14 +12,10 @@ namespace zd5
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Welcome : ContentPage
     {
-        string login;
         public Welcome(string _login)
         {
             InitializeComponent();
             welcomeLabel.Text = "Добро пожаловать, " + _login;
-            login = _login;
         }
-
-        
     }
 }
